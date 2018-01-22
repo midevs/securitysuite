bc6c8a76c4e574ad2cc06ac26b4ed83e2c72aa1d
alphafantomu <phantomcrazyheart@gmail.com>
2018-01-21 20:32:23 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -48,8 +48,8 @@ function FileLibrary:AttachWatchHandler(Key, Object)
 	if not Object then return end 
 	
 	for _,Handler in ipairs(ExistingFileConnections) do 
-		local PotentialObject = Handler[1]
-		if PotentialObject == Object then 
+		local PotentialObject = Handler[1] --got string value, Object.Name
+		if PotentialObject == Object.Name then  --got instance, Object, attempt to compare Object with a string, will always be false.
 			print("Killing duplicate object") -- DEBUG: NEVER GET THIS PRINT
 			return -- This will kill the function 
 		end
